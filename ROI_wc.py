9ec3299f6c03f5351909568b0583522bf95f85ae
Valia <50623402+PikachuDeveloper@users.noreply.github.com>
2021-08-10 10:21:32 +0300

    Update ROI_wc.py

--- ROI_wc.py
+++ ROI_wc.py
@@ -15,7 +15,7 @@ drawing = False
 
 # coordinates (x0, y0), (x1, y1) of the top left and
 # bottom right corners to draw a ROI
-Rect = [(1, 1), (2, 2)]
+Rect = [(0, 0), (2, 2)]
 
 #image, that will be shown in a frame
 img = np.zeros((512,512,3), np.uint8)
@@ -28,27 +28,22 @@ mode = False
 #when Lbutton down - we draw ROI
 #when Lbutton up - ROI is on the screen
 #when Rbutton clicked - rectangle should disappear
-def draw(event,x,y,flags,param):
-    global drawing, Rect, mode, img
+def draw(event,x,y, flags, params):
+    global drawing, Rect, mode
     if event == cv2.EVENT_LBUTTONDOWN:
         mode = True
         drawing = True
         Rect[0], Rect[1] = (x, y), (x, y)
     elif event == cv2.EVENT_MOUSEMOVE and drawing:
         Rect[1] = (x, y)
-        img = cv2.rectangle(img,Rect[0],(x,y),(0,255,0),0)
-        cv2.imshow(wTitle, img)
 
     elif event == cv2.EVENT_LBUTTONUP:
         mode = True
         drawing = False
         Rect[1] = (x, y)
-        img = cv2.rectangle(img,Rect[0],Rect[1],(0,255,0),0)
-        cv2.imshow(wTitle, img)
 
-    elif event == cv2.EVENT_RBUTTONDOWN:
+    elif event == cv2.EVENT_RBUTTONUP:
         mode = False
-        drawing = False
 
 
 vid = cv2.VideoCapture(0)
